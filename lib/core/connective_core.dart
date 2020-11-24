d131c5f39ad20ed0c19fb418450281cf9d239e43
Cybernetics Core <corecybernetics354@gmail.com>
2020-11-24 15:59:34 +0700

    Adding stream

--- lib/core/connective_core.dart
+++ lib/core/connective_core.dart
@@ -1,6 +1,16 @@
 part of connective;
 
+enum ConnectiveStatus {online, offline}
+
 class Connective {
+  final _connectiveStatusController = new StreamController<ConnectiveStatus>.broadcast();
+  Stream<ConnectiveStatus> get connectiveStream => _connectiveStatusController.stream;
+  StreamSink<ConnectiveStatus> get _connectiveIn => _connectiveStatusController.sink;
+
+  final _connectiveTypeController = new StreamController<ConnectivityResult>.broadcast();
+  Stream<ConnectivityResult> get connectiveType => _connectiveTypeController.stream;
+  StreamSink<ConnectivityResult> get _connectiveTypeIn => _connectiveTypeController.sink;
+
   ConnectiveConfiguration _configuration;
   static final Connective _singleton = Connective._();
   Connective._();
@@ -20,6 +30,7 @@ class Connective {
   }
 
   _handleNetwork(ConnectivityResult result) {
+    _connectiveTypeIn.add(result);
     switch (result) {
       case ConnectivityResult.mobile: {
         _configuration.onMobileData();
@@ -55,13 +66,16 @@ class Connective {
     try {
       final res = await InternetAddress.lookup(_configuration.url).timeout(_configuration.timeout);
       if(res.isNotEmpty && res[0].rawAddress.isNotEmpty) {
+        _connectiveIn.add(ConnectiveStatus.online);
         _configuration.whenOnline();
       } else {
+        _connectiveIn.add(ConnectiveStatus.offline);
         _configuration.whenOffline();
         await Future.delayed(_configuration.delay);
         _lookupAddress();
       }
     } catch (e) {
+      _connectiveIn.add(ConnectiveStatus.offline);
       _configuration.whenOffline();
       await Future.delayed(_configuration.delay);
       _lookupAddress();
@@ -70,5 +84,7 @@ class Connective {
 
   void dispose() {
     _connectionSubscribe?.cancel();
+    _connectiveStatusController?.close();
+    _connectiveTypeController?.close();
   }
 }
